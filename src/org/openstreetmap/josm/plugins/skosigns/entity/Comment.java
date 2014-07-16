7dd3cfa3c20bca17289a40e8e803f83021b6fc2e
beata.jancso <beata.jancso@702c9929-26ad-4ec3-b8b7-334534ed37ec>
2014-07-16 13:14:49 +0000

    - defined project entitites (http://jira.skobbler.de:8080/browse/JBOSCH-17)
    git-svn-id: svn://svn-srv/skosigns/trunk@6 702c9929-26ad-4ec3-b8b7-334534ed37ec

---
+++ src/org/openstreetmap/josm/plugins/skosigns/entity/Comment.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014, skobbler GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Created on Jul 16, 2014 by Beata
+ * Modified on $DateTime$ by $Author$
+ */
+package org.openstreetmap.josm.plugins.skosigns.entity;
+
+
+/**
+ * Defines the comment business entity.
+ * 
+ * @author Beata
+ * @version $Revision$
+ */
+public class Comment {
+    
+    private String userName;
+    private Long tstamp;
+    private String text;
+    private Status status;
+    private Long duplicateOf;
+    
+    /**
+     * Builds a new object with the given arguments.
+     * 
+     * @param userName the OSM user name of the user who posted the comment
+     * @param text the text of the comment
+     * @param status the {@code Status} set to the road sign by this comment.
+     * This attribute may be missing if the comment has not changed the status.
+     * @param duplicateOf the id of the road sign of which this road sign is a
+     * duplicate of. This attribute is only present if the status is
+     * {@code Status#DUPLICATE}
+     */
+    public Comment(String userName, String text, Status status, Long duplicateOf) {
+        this.userName = userName;
+        this.text = text;
+        this.status = status;
+        this.duplicateOf = duplicateOf;
+    }
+    
+    
+    /**
+     * Builds a new object with the given arguments.
+     * 
+     * @param userName the OSM user name of the user who posted the comment
+     * @param tstamp the timestamp at which the comment has been posted, given
+     * in "Unix time" format
+     * @param text the text of the comment
+     * @param status the {@code Status} set to the road sign by this comment.
+     * This attribute may be missing if the comment has not changed the status.
+     * @param duplicateOf the id of the road sign of which this road sign is a
+     * duplicate of. This attribute is only present if the status is
+     * {@code Status#DUPLICATE}
+     */
+    public Comment(String userName, Long tstamp, String text, Status status,
+            Long duplicateOf) {
+        this(userName, text, status, duplicateOf);
+    }
+    
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public Long getTstamp() {
+        return tstamp;
+    }
+    
+    public String getText() {
+        return text;
+    }
+    
+    public Status getStatus() {
+        return status;
+    }
+    
+    public Long getDuplicateOf() {
+        return duplicateOf;
+    }
+}
